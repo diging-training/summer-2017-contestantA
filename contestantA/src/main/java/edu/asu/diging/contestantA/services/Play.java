ee7bf47dbd3d8f5b291902d0e5ae368a849197dc
Azhaku Sakthi Vel M <jatsakthi@gmail.com>
2017-05-25 12:14:34 -0700

    Updated Play.java

--- contestantA/src/main/java/edu/asu/diging/contestantA/services/Play.java
+++ contestantA/src/main/java/edu/asu/diging/contestantA/services/Play.java
@@ -1,6 +1,5 @@
-package edu.asu.diging.contestantA.services;
+package edu.asu.diging.tutorial.spring.service;
 
-<<<<<<< HEAD
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.LinkedList;
@@ -11,16 +10,10 @@ import java.util.Scanner;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-=======
-import java.util.ArrayList;
-import java.util.List;
-
->>>>>>> 28e1e71eae36f00a304bc7d262b5886a0dce5abc
 import org.springframework.stereotype.Service;
 
 @Service
 public class Play {
-<<<<<<< HEAD
 	@Autowired
 	private Sender send;
 	final static Logger logger = Logger.getLogger(Play.class);
@@ -154,41 +147,4 @@ public class Play {
 		
 	}
 	
-=======
-	
-	public List<Integer> playerOne;
-	public List<Integer> playerTwo;
-	public int currentNumber;
-	
-	public Play() {
-		playerOne = new ArrayList<Integer>();
-		playerOne.add(1);
-		playerOne.add(3);
-		playerOne.add(5);
-		playerOne.add(7);
-		playerOne.add(9);
-		playerTwo = new ArrayList<Integer>();
-		playerTwo.add(2);
-		playerTwo.add(4);
-		playerTwo.add(6);
-		playerTwo.add(8);
-		playerTwo.add(10);
-		currentNumber = 50;
-	}
-	
-	public List<Integer> getPlayerOne(){
-		return playerOne;
-	}
-	
-	public List<Integer> getPlayerTwo(){
-		return playerTwo;
-	}
-	
-	public int getCurrentNumber(){
-		return currentNumber;
-	}
-    
-
-
->>>>>>> 28e1e71eae36f00a304bc7d262b5886a0dce5abc
 }

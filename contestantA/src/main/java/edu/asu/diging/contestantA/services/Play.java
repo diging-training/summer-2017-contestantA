26c0e9fd663d984b67b2907a4ef28cc521957f61
jatsakthi <jatsakthi@gmail.com>
2017-05-26 14:07:48 -0700

    asdfasdf

--- contestantA/src/main/java/edu/asu/diging/contestantA/services/Play.java
+++ contestantA/src/main/java/edu/asu/diging/contestantA/services/Play.java
@@ -5,7 +5,6 @@ import java.util.Random;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
